933c8879d21f34ced6a9531fc0f03049014edb04
Cody <cjgordon@gmail.com>
2017-03-25 20:10:00 +0700

    api token host

--- routes/api/token.js
+++ routes/api/token.js
@@ -10,7 +10,7 @@ router.get('/', (req, res, next) => {
   const prodEnv = process.env.NODE_ENV === 'production'
   const prodHost = 'showspark.herokuapp.com'
   if ((!prodEnv && req.hostname === 'localhost') ||
-  (!!prodEnv && req.hostname === prodHost)) {
+  (prodEnv && req.hostname === prodHost)) {
     const options = {
       method: 'POST',
       url: `${process.env.AUTH0_ISSUER}oauth/token`,
