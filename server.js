76de428440323b7c786a502e6b39f6a8d83c7be5
Cody <cjgordon@gmail.com>
2017-04-08 11:42:03 +0900

    fixed prod resource serving

--- server.js
+++ server.js
@@ -50,6 +50,7 @@ mongo.connectToServer(() => {
 
     /* serve all routes via react build if in prod */
     if (process.env.NODE_ENV === 'production') {
+      app.use(express.static('client/build'))
       app.get('*', (req, res) => {
         res.sendFile(path.resolve(`${__dirname}/client/build/index.html`))
       })
