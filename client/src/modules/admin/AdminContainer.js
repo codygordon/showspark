d8745f6ca006e56032859c38b3fc8a41f9f6ec22
Cody <cjgordon@gmail.com>
2017-07-14 19:37:22 -0700

    admin module start

---
+++ client/src/modules/admin/AdminContainer.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import qs from 'query-string'
+
+import apiCall from '../../utils/api-call'
+
+export default class AdminContainer extends Component {
+  static propTypes = {
+
+  }
+
+  state = {
+
+  }
+
+  componentWillMount() {
+
+  }
+
+  componentWillReceiveProps(nextProps) {
+
+  }
+
+  render() {
+    return (
+      <section className="admin-container">
+        <h1>Admin Panel</h1>
+      </section>
+    )
+  }
+}
---
+++ client/src/modules/admin/components/ArtistRow.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ArtistRow = props => (
+  <div className="artist-row" />
+)
+
+ArtistRow.propTypes = {
+
+}
+
+export default ArtistList
